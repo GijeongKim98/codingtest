3cd520b279d203411fa05774893c0770f70254d6
GijeongKim98 <kkj981814@gmail.com>
2023-09-21 18:41:00 +0900

    [Add] BOJ 11404 : Floyd gold 4

---
+++ 백준/graph/Floyd.py
@@ -0,0 +1,31 @@
+# 플로이드
+"""https://www.acmicpc.net/problem/11404"""
+
+import sys
+
+INF = 100000000
+
+try:
+    n, m = [int(sys.stdin.readline()) for _ in range(2)]
+    dist = [[0 if i==j else INF for i in range(n)] for j in range(n)]
+    
+    for _ in range(m):
+        s, e, c = map(int, sys.stdin.readline().split(" "))
+        dist[s-1][e-1] = min(dist[s-1][e-1], c)
+    
+    
+    for k in range(n):
+        for i in range(n):
+            if i == k : continue
+            for j in range(n):
+                if j == k or j == i : continue
+                
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+                
+    
+    for row in dist:
+        print(*row)
+    
+    
+except ValueError or IndexError as e:
+    print(e)
